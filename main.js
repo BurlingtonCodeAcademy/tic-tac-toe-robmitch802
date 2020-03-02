b7e236f7b101e3d8ccb434371ca5025b99b33081
robmitch802 <60521912+robmitch802@users.noreply.github.com>
2020-03-02 13:26:33 -0500

    latest changes

--- main.js
+++ main.js
@@ -1,36 +1,11 @@
 //Tic-Tac-Toe 2.20.20
 
-//start game
-//->user clicks on "start"
-//--start button grayed out --status area says "Player X's turn"
-
-// document.getElementById('start').on('click', ':button', function (e) {
-//     let btn = $(e.target);
-//     btn.attr("disabled", "disabled"); // disable button
-
-// });
-
-
-//current player
-//->user selects cell
-//--board redraws --x (or O) appears in that cell
-//--turn ends --current player goes to opposite (x or o)
-// onclick document.getElementById()
-//     ->sends cell value to winCheck; 
-//     ->replaces empty cell with x or o
-//     ->playerTurn ++
-
-//user select cell
-//->user picks an already-filled cell
-//--game says "Pleaase select an empty cell" 
-//--current player does _not_ change
 
 //win condition
 //-> player achieves 3 xoro in a row, column or diagonal
 //--system draws line through three winning three cells
 //--App says "congratulations!"
 //--Game ends
-
 
 //name picking
 //-> when starting a game, user can pick player names
@@ -72,6 +47,10 @@ let startInit = false
 
 let playerTurn = "playerX"
 
+let playerOneName = "Player X"
+
+let playerTwoName = "Player O"
+
 //-------------cell objects -----------------------------//
 let cellZero = new cellObject("zero", "cell-0", false, false, "corner")
 let cellOne = new cellObject("one", "cell-1", false, false, "side")
@@ -110,30 +89,41 @@ const winCells = {
 function winCheck(playerArray) {
     if (playerArray.includes(winCells.one[0]) && playerArray.includes(winCells.one[1]) && playerArray.includes(winCells.one[2])) {
         console.log("array one wins check successful")
-        document.getElementById('statusBoard').innerHTML = "Player " + playerTurn + "WINS!!!"
+        document.getElementById('statusBoard').innerHTML = "Player " + playerTurn + " WINS!!!"
+        document.getElementById('whoseTurn').innerHTML = "Congratulations!!!"
     } else if (playerArray.includes(winCells.two[0]) && playerArray.includes(winCells.two[1]) && playerArray.includes(winCells.two[2])) {
         console.log("array two wins check successful")
-        document.getElementById('statusBoard').innerHTML = "Player " + playerTurn + "WINS!!!"
+        document.getElementById('statusBoard').innerHTML = "Player " + playerTurn + " WINS!!!"
+        document.getElementById('whoseTurn').innerHTML = "Congratulations!!!"
     } else if (playerArray.includes(winCells.three[0]) && playerArray.includes(winCells.three[1]) && playerArray.includes(winCells.three[2])) {
-        console.log("array two wins check successful")
-        document.getElementById('statusBoard').innerHTML = "Player " + playerTurn + "WINS!!!"
+        console.log("array three wins check successful")
+        document.getElementById('statusBoard').innerHTML = "Player " + playerTurn + " WINS!!!"
+        document.getElementById('whoseTurn').innerHTML = "Congratulations!!!"
     } else if (playerArray.includes(winCells.four[0]) && playerArray.includes(winCells.four[1]) && playerArray.includes(winCells.four[2])) {
-        console.log("array two wins check successful")
-        document.getElementById('statusBoard').innerHTML = "Player " + playerTurn + "WINS!!!"
+        console.log("array four wins check successful")
+        document.getElementById('statusBoard').innerHTML = "Player " + playerTurn + " WINS!!!"
+        document.getElementById('whoseTurn').innerHTML = "Congratulations!!!"
     } else if (playerArray.includes(winCells.five[0]) && playerArray.includes(winCells.five[1]) && playerArray.includes(winCells.five[2])) {
-        console.log("array two wins check successful")
-        document.getElementById('statusBoard').innerHTML = "Player " + playerTurn + "WINS!!!"
+        console.log("array five wins check successful")
+        document.getElementById('statusBoard').innerHTML = "Player " + playerTurn + " WINS!!!"
+        document.getElementById('whoseTurn').innerHTML = "Congratulations!!!"
     } else if (playerArray.includes(winCells.six[0]) && playerArray.includes(winCells.six[1]) && playerArray.includes(winCells.six[2])) {
-        console.log("array two wins check successful")
-        document.getElementById('statusBoard').innerHTML = "Player " + playerTurn + "WINS!!!"
+        console.log("array six wins check successful")
+        document.getElementById('statusBoard').innerHTML = "Player " + playerTurn + " WINS!!!"
+        document.getElementById('whoseTurn').innerHTML = "Congratulations!!!"
     } else if (playerArray.includes(winCells.seven[0]) && playerArray.includes(winCells.seven[1]) && playerArray.includes(winCells.seven[2])) {
-        console.log("array two wins check successful")
-        document.getElementById('statusBoard').innerHTML = "Player " + playerTurn + "WINS!!!"
+        console.log("array seven wins check successful")
+        document.getElementById('statusBoard').innerHTML = "Player " + playerTurn + " WINS!!!"
+        document.getElementById('whoseTurn').innerHTML = "Congratulations!!!"
     } else if (playerArray.includes(winCells.eight[0]) && playerArray.includes(winCells.eight[1]) && playerArray.includes(winCells.eight[2])) {
-        console.log("array two wins check successful")
-        document.getElementById('statusBoard').innerHTML = "Player " + playerTurn + "WINS!!!"
-    }
+        console.log("array eight wins check successful")
+        document.getElementById('statusBoard').innerHTML = "Player " + playerTurn + " WINS!!!"
+        document.getElementById('whoseTurn').innerHTML = "Congratulations!!!"
+    } else {console.log("no winners here yet")}
 }
+
+
+//*
 
 //--------------------function to change cell status, place x or o---------//
 function cellMove(cellID) {
@@ -141,26 +131,28 @@ function cellMove(cellID) {
     console.log(cellID)
     if (cellLookup[cellID].takenO === true || cellLookup[cellID].takenX === true) {
         document.getElementById('statusBoard').innerHTML = "That square is taken! Try a different move."
+    } else if (startInit === false) {
+        document.getElementById('statusBoard').innerHTML = "You haven't started the game yet. Choose one-player or two-player."
     } else if (playerTurn === "playerO") {
         cellLookup[cellID].takenO = true; //sets taken status to true
         document.getElementById(cellLookup[cellID].ID).innerHTML = "O";
-        document.getElementById('statusBoard').innerHTML = ("Player O took cell " + cellLookup[cellID].ID);
+        document.getElementById('statusBoard').innerHTML = (playerTwoName + " took cell " + cellLookup[cellID].ID);
         playerOList.push(cellLookup[cellID].ID)
         winCheck(playerOList);
         playerTurn = "playerX" //switches player turn to other player
         console.log("inside else if " + playerTurn)
-        document.getElementById('whoseTurn').innerHTML = "It's " + playerTurn + "'s turn."
+        document.getElementById('whoseTurn').innerHTML = "It's " + playerOneName + "'s turn."
         console.log("Player X moves: " + playerXList)
         console.log("Player O moves: " + playerOList)
     } else {
         cellLookup[cellID].takenX = true;
         document.getElementById(cellLookup[cellID].ID).innerHTML = "X";
         playerXList.push(cellLookup[cellID].ID) //adds player move to player move list
-        document.getElementById('statusBoard').innerHTML = ("Player X took cell " + cellLookup[cellID].ID);
+        document.getElementById('statusBoard').innerHTML = (playerOneName + " took cell " + cellLookup[cellID].ID);
         winCheck(playerXList)
         playerTurn = "playerO" //switches player turn to other player
         console.log("inside else for " + playerTurn)
-        document.getElementById('whoseTurn').innerHTML = "It's " + playerTurn + "'s turn."
+        document.getElementById('whoseTurn').innerHTML = "It's " + playerTwoName + "'s turn."
         console.log("Player X moves: " + playerXList)
         console.log("Player O moves: " + playerOList)
     }
@@ -195,28 +187,29 @@ document.getElementById('cell-8').addEventListener('click', () => {
     cellMove('cellEight');
 }, 2000)
 
-
-//------------------------start button event listener --------------------//
-
-//one-player button 
+//---------------------------one-player game start----------------------//
 document.getElementById('one-player').addEventListener('click', () => {
     onePlayerInit();
 });
 function onePlayerInit() {
     document.getElementById('statusBoard').innerHTML = "The computer is ready. You are X, computer is O - X goes first.";
+    startInit = true;
+    playerTwoName = "Computer"
+    playerOneName = "Human Being"
     setInterval(gameTimer, 1000)
 }
 
-//two-player button
+//--------------------two-player game start ------------------------//
 document.getElementById('two-player').addEventListener('click', () => {
     twoPlayerInit();
 });
 
 function twoPlayerInit() {
     document.getElementById('statusBoard').innerHTML = "Player One, it's your turn, go first.";
+    startInit = true;
     setInterval(gameTimer, 1000)
 }
-//game clock
+//-------------------- game clock -----------------------------------// 
 let time = 0
 let timeOverflow = 0
 function gameTimer() {
@@ -229,6 +222,7 @@ function gameTimer() {
     }
 }
 
+//-------------random integer generator for computer dumb AI story --------------//
 function randomInt(max, min) {
     return Math.floor(min + (Math.random() * (max - min + 1)))
 }
